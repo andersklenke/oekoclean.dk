3a73f7bafcc80d6cd4e0da667a24325c92a7f51d
Anders Klenke <anders_klenke@icloud.com>
2017-10-22 11:28:13 +0200

    Add proper GA tracking

--- js/form-submission-handler.js
+++ js/form-submission-handler.js
@@ -51,6 +51,7 @@ function handleFormSubmit(event) {  // handles form submit withtout any jquery
       console.log(xhr.responseText);
       document.getElementById('form-section').style.display = 'none'; // hide form
       document.getElementById('thankyou_message').style.display = 'block';
+      ga('send', 'event', 'Contact form', 'submitted');
       return;
   };
   // url encode form data for sending as post data
